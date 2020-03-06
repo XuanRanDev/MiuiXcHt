c9565f56c7c91024bef46042f372c8f7509183fc
XuanRan0808 <3135419729@qq.com>
2020-03-07 00:41:34 +0800

    第一次提交

--- app/src/main/java/com/XuanRan/MiuiXcHt/Main.java
+++ app/src/main/java/com/XuanRan/MiuiXcHt/Main.java
@@ -2,6 +2,7 @@ package com.XuanRan.MiuiXcHt;
 
 import de.robv.android.xposed.*;
 import de.robv.android.xposed.callbacks.*;
+import de.robv.android.xposed.XC_MethodHook.*;
 
 
 public class Main implements IXposedHookLoadPackage
@@ -9,9 +10,19 @@ public class Main implements IXposedHookLoadPackage
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable
     {
-        if(loadPackageParam.packageName.equals("")){
-            Class<?> clazz=XposedHelpers.findClass("",loadPackageParam.classLoader);
+        if(loadPackageParam.packageName.equals("com.miui.gallery")){
             
+            XposedHelpers.findAndHookMethod("com.miui.gallery.editor.photo.app.filter.skytransfer.SkyLibResVersionHelper",loadPackageParam.classLoader,"isSupportDynamicSkylnner",new XC_MethodHook(){
+                   
+                     @Override
+                    protected void afterHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable
+                      {
+                        super.afterHookedMethod(param);
+                        param.setResult(true);
+                    }
+            });
         }
+        
     }
 }
+
